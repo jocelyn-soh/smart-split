45af78b1fc70a9b9df58979c11443ac2b28a133f
jocelyn-soh <s.sohjoce@gmail.com>
2024-03-10 02:49:09 +0800

    Update Person.java

--- src/main/Person.java
+++ src/main/Person.java
@@ -1,11 +1,15 @@
 package src.main;
 
+import java.util.ArrayList;
+
 /**
  * Represents a person with a name and amount paid.
  */
 public class Person {
     private final String name;
     private double amtPaid;
+
+    private static ArrayList<Person> peopleList = new ArrayList<>();
 
     public Person(String name, double amtPaid) {
         this.name = name;
